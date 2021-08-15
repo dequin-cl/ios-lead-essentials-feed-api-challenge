87d734a57560f3602c457d977294484281f7fa90
Iván GalazJeria <dequin@gmail.com>
2021-08-15 12:46:45 +0200

    Delivers FeedImage with 200 response and valid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,8 +22,8 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				if response.statusCode == 200, let _ = try? JSONDecoder().decode(Root.self, from: data) {
-					completion(.success([]))
+				if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+					completion(.success(root.items.map { $0.item }))
 				} else {
 					completion(.failure(Error.invalidData))
 				}
@@ -34,13 +34,21 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	private struct Root: Decodable {
-		let items: [FeedItemAPI]
+		let items: [Image]
 	}
 
-	private struct FeedItemAPI: Decodable {
-		let image_id: String
-		let image_desc: String
-		let image_loc: String
+	private struct Image: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
 		let image_url: URL
+
+		var item: FeedImage {
+			FeedImage(
+				id: image_id,
+				description: image_desc,
+				location: image_loc,
+				url: image_url)
+		}
 	}
 }
